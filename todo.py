9a4956c102af6e18673c9b23481aec89ac561fd6
chandlereskey <chaneskey@gmail.com>
2024-12-21 23:14:16 -0500

    trying port 8000

--- todo.py
+++ todo.py
@@ -109,4 +109,4 @@ def complete_task(n_clicks, current_tasks):
 
 # Run the app
 if __name__ == "__main__":
-    serve(app.server, host="0.0.0.0", port=8080)
+    serve(app.server, host="0.0.0.0", port=8000)
